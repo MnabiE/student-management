a9bdb5ca312a51e9766369bb53d7076d1d543c1c
Mohammad Nabi Ebrahimi <mnabie2024@gmail.com>
2025-09-11 00:00:32 +0430

    the buttons added

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function addStudent() {
+    let name = document.getElementById("userName").value;
+    if (name == "" || name == " " || name == "  "){
+        alert("Enter valid data!")
+    }
+}
